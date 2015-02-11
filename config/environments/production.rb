76591a95eab9313ef522d7241595a0b9112a08c8
Chip Neff <chiza007@yahoo.com>
2015-02-11 09:31:21 -0800

    Add Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,15 @@ Rails.application.configure do
   # Required for Heroku
   # Note to et to actual Host
 config.action_mailer.default_url_options = { host: 'chip-pinteresting.herokuapp.com', port: 3000 }
+  
+  #Sets paperclip to Amzazon s3
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 
 end
